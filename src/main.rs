2c95d397dee8730e040e3e9dc053ed2b0cb6cc29
Toby <toby@hermes.local>
2023-05-28 15:21:31 -0600

    +camera and floor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello, world!");
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Floor
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
+        material: materials.add(Color::SILVER.into()),
+        ..default()
+    });
+
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 6.0, 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        ..default()
+    });
 }
