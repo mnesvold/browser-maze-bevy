7e59671d5cf82c1febbb786dcda3bb8702d347c8
Michael Nesvold <mdnesvold@gmail.com>
2023-06-25 12:57:53 -0600

    +WASM build script

---
+++ build-wasm.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+cd $(dirname $0)
+
+if ! which wasm-bindgen >/dev/null 2>/dev/null
+then
+    echo >&2 "Can't find \`wasm-bindgen\` on path."
+    echo >&2 "Do you need to call \`cargo install [--root ...] wasm-bindgen-cli\` first?"
+    exit 1
+fi
+
+cargo build \
+    --release \
+    --target wasm32-unknown-unknown
+
+wasm-bindgen \
+    --out-name browser_maze_bevy \
+    --out-dir www/scripts \
+    --target web \
+    target/wasm32-unknown-unknown/release/browser-maze-bevy.wasm
+
+find www -type f -name '*.ts' -delete
+
+echo "Success! Now copy \`./www\` to your web root."
